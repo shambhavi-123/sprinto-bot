7c603699b22c954072b1610accc40be69d304f90
shouryaa28 <shrivastavshourya28@gmail.com>
2025-09-13 12:10:17 +0530

    change

--- routes/address.ts
+++ routes/address.ts
@@ -11,7 +11,7 @@ import { AddressModel } from '../models/address'
 //     const addresses = await AddressModel.findAll({ where: { UserId: req.body.UserId } })
 //     res.status(200).json({ status: 'success', data: addresses })
 //   }
-}
+// }
 
 module.exports.getAddressById = function getAddressById () {
   return async (req: Request, res: Response) => {
